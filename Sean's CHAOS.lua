823fbc8dbd76d1136eb9c8c3531469c7e6cdd877
seanthetoiletgamer <106478896+seanthetoiletgamer@users.noreply.github.com>
2022-05-30 17:09:45 +0700

    new cool script for chaos game

---
+++ Sean's CHAOS.lua
@@ -0,0 +1,80 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Sean's CHAOS | sean pro toilet gamer#5554", "DarkTheme")
+
+--Main
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Main") --Main Tab
+Section:NewButton("Hitbox", "Make enemies easier to kill with bigger hitbox", function()
+    _G.HeadSize = 20
+	_G.Disabled = true
+ 
+	game:GetService('RunService').RenderStepped:connect(function()
+		if _G.Disabled then
+			for i,v in next, game:GetService('Players'):GetPlayers() do
+				if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+					pcall(function()
+						v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
+						v.Character.HumanoidRootPart.Transparency = 0.7
+						v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Bright red")
+						v.Character.HumanoidRootPart.Material = "Neon"
+						v.Character.HumanoidRootPart.CanCollide = false
+					end)
+				end
+			end
+		end
+	end)
+end)
+Section:NewButton("Gamepasses", "Get the red knife for free without wasting a single penny", function()
+    if game.CreatorType == Enum.CreatorType.User then
+		game.Players.LocalPlayer.UserId = game.CreatorId
+	end
+	if game.CreatorType == Enum.CreatorType.Group then
+		game.Players.LocalPlayer.UserId = game:GetService("GroupService"):GetGroupInfoAsync(game.CreatorId).Owner.Id
+	end
+end)
+
+--Player
+local Tab = Window:NewTab("Player")
+local Section = Tab:NewSection("Player")
+Section:NewSlider("Walkspeed", "Walk like flash or sonic", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+Section:NewSlider("Jumppower", "Jump higher lol", 500, 0, function(v) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
+end)
+Section:NewButton("Infinite Jump", "Press F to Infinite Jump", function()
+    _G.infinjump = true
+
+    local Player = game:GetService("Players").LocalPlayer
+    local Mouse = Player:GetMouse()
+    Mouse.KeyDown:connect(function(k)
+    if _G.infinjump then
+    if k:byte() == 32 then
+    Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+    Humanoid:ChangeState("Jumping")
+    wait(0.1)
+    Humanoid:ChangeState("Seated")
+    end
+    end
+    end)
+    
+    local Player = game:GetService("Players").LocalPlayer
+    local Mouse = Player:GetMouse()
+    Mouse.KeyDown:connect(function(k)
+    k = k:lower()
+    if k == "f" then
+    if _G.infinjump == true then
+    _G.infinjump = false
+    else
+    _G.infinjump = true
+    end
+    end
+    end)
+end)
+
+--Credits
+local Tab = Window:NewTab("Credits")
+local Section = Tab:NewSection("Credits")
+Section:NewButton("sean pro toilet gamer#5554", "Click The button to copy", function()
+setclipboard("sean pro toilet gamer#5554")
+end)
