d943b5d30b77fc5610e02b2ba1ef2b0f0d678008
seanthetoiletgamer <106478896+seanthetoiletgamer@users.noreply.github.com>
2022-06-01 15:11:29 +0700

    fixed the infinite jump

--- Sean's CHAOS.lua
+++ Sean's CHAOS.lua
@@ -42,34 +42,30 @@ end)
 Section:NewSlider("Jumppower", "Jump higher lol", 500, 0, function(v) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
 end)
-Section:NewButton("Infinite Jump", "Press F to Infinite Jump", function()
-    _G.infinjump = true
-
-    local Player = game:GetService("Players").LocalPlayer
-    local Mouse = Player:GetMouse()
-    Mouse.KeyDown:connect(function(k)
-    if _G.infinjump then
-    if k:byte() == 32 then
-    Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-    Humanoid:ChangeState("Jumping")
-    wait(0.1)
-    Humanoid:ChangeState("Seated")
-    end
-    end
-    end)
-    
-    local Player = game:GetService("Players").LocalPlayer
-    local Mouse = Player:GetMouse()
-    Mouse.KeyDown:connect(function(k)
-    k = k:lower()
-    if k == "f" then
-    if _G.infinjump == true then
-    _G.infinjump = false
+Section:NewToggle("Infinite Jump", "Jump Infinitely, he just has a good gaming chair", function(state)
+    if state then
+_G.infinjump = true
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+Mouse.KeyDown:connect(function(k)
+if _G.infinjump then
+if k:byte() == 32 then
+Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+Humanoid:ChangeState("Jumping")
+wait(0.1)
+Humanoid:ChangeState("Seated")
+end
+end
+end)
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
     else
-    _G.infinjump = true
-    end
-    end
-    end)
+if _G.infinjump == true then
+_G.infinjump = false
+else
+_G.infinjump = true
+end
+end
 end)
 
 --Credits
