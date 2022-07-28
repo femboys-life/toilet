42107934c1400776462491ac9e0c36040a540272
seanthetoiletgamer <106478896+seanthetoiletgamer@users.noreply.github.com>
2022-07-28 22:50:31 +0700

    loadstring

---
+++ Loadstring.lua
@@ -0,0 +1,28 @@
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/seanthetoiletgamer/RobloxScripts/main/Loadstring.lua"))()
+--Aimbot might also work on some other games
+--Cart ride script might also work on other cart games
+
+if game.PlaceId == 8059519018
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/seanthetoiletgamer/RobloxScripts/main/Sean's%20Aimbot.lua"))()
+end
+if game.PlaceId == 8523408215
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/seanthetoiletgamer/RobloxScripts/main/Sean's%20ALO%20Sanctuary.lua"))()
+end
+if game.PlaceId == 3851622790
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/seanthetoiletgamer/RobloxScripts/main/Sean's%20Break%20In.lua"))()
+end
+if game.PlaceId == 6441847031
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/seanthetoiletgamer/RobloxScripts/main/Sean's%20CHAOS.lua"))()
+end
+if game.PlaceId == 4913581664
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/seanthetoiletgamer/RobloxScripts/main/Sean's%20Cart%20Ride%20Into%20Rdite.lua"))()
+end
+if game.PlaceId == 205224386
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/seanthetoiletgamer/RobloxScripts/main/Sean's%20Hide%20and%20Seek%20Extreme.lua"))()
+end
+if game.PlaceId == 1962086868
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/seanthetoiletgamer/RobloxScripts/main/Sean's%20Tower%20of%20Hell.lua"))()
+end
+if game.PlaceId == 1240123653
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/seanthetoiletgamer/RobloxScripts/main/Sean's%20Zombie%20Attack.lua"))()
+end
